116242eaf16ed91ef95a5bec228cc99180398e37
iulia <juliaszuhai@gmail.com>
2019-03-20 10:45:34 +0200

    Initial commit

---
+++ src/main/java/VVSS_SzuhaiBoanca/App.java
@@ -0,0 +1,13 @@
+package VVSS_SzuhaiBoanca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
