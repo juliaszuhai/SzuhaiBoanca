289f89211148fda446c695f847c9155fd6327c0c
iulia <juliaszuhai@gmail.com>
2019-03-20 11:20:43 +0200

    Tests added

---
+++ src/main/java/VVSS_SzuhaiBoanca/app/MainApplication.java
@@ -0,0 +1,36 @@
+package VVSS_SzuhaiBoanca.app;
+
+
+import VVSS_SzuhaiBoanca.repository.NotaXMLRepo;
+import VVSS_SzuhaiBoanca.repository.StudentXMLRepo;
+import VVSS_SzuhaiBoanca.repository.TemaXMLRepo;
+import VVSS_SzuhaiBoanca.service.Service;
+import VVSS_SzuhaiBoanca.validation.NotaValidator;
+import VVSS_SzuhaiBoanca.validation.StudentValidator;
+import VVSS_SzuhaiBoanca.validation.TemaValidator;
+import VVSS_SzuhaiBoanca.view.UI;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+       //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        UI ui = new UI(service);
+        ui.run();
+    }
+
+}
---
+++ src/test/java/VVSS_SzuhaiBoanca/service/ServiceTest.java
@@ -0,0 +1,57 @@
+package VVSS_SzuhaiBoanca.service;
+
+import VVSS_SzuhaiBoanca.domain.Student;
+import VVSS_SzuhaiBoanca.repository.NotaXMLRepo;
+import VVSS_SzuhaiBoanca.repository.StudentXMLRepo;
+import VVSS_SzuhaiBoanca.repository.TemaXMLRepo;
+import VVSS_SzuhaiBoanca.validation.NotaValidator;
+import VVSS_SzuhaiBoanca.validation.StudentValidator;
+import VVSS_SzuhaiBoanca.validation.TemaValidator;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+public class ServiceTest {
+
+    @Test
+    public void addStudent() {
+        Student a=new Student("1","Sara",931,"sara@gmail.com");
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent="fisiere/StudentiTest.xml";
+        String filenameTema = "fisiere/TemeTest.xml";
+        String filenameNota = "fisiere/NoteTest.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        service.addStudent(a);
+        int i = 0;
+        Iterator it=service.getAllStudenti().iterator();
+        while(it.hasNext()) {
+            i++;
+            it.next();
+        }
+        assertEquals(1, i);
+        assertEquals(a.getNume(),service.findStudent("1").getNume());
+    }
+    @Test
+    public void addStudent2() {
+        Student a=new Student("1","Sara",931,"sara@gmail.com");
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent="fisiere/StudentiTest.xml";
+        String filenameTema = "fisiere/TemeTest.xml";
+        String filenameNota = "fisiere/NoteTest.xml";
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        service.addStudent(a);
+        assertEquals(a.getNume(),service.findStudent("1").getNume());
+    }
+}
