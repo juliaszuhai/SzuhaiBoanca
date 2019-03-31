294b24afda33542fc45c0c26476a57830a9cb113
saboanca <sara_boanca@yahoo.com>
2019-03-31 18:00:26 +0300

    Add tests with Before annotation.

--- src/test/java/VVSS_SzuhaiBoanca/service/ServiceTest.java
+++ src/test/java/VVSS_SzuhaiBoanca/service/ServiceTest.java
@@ -7,6 +7,7 @@ import VVSS_SzuhaiBoanca.repository.TemaXMLRepo;
 import VVSS_SzuhaiBoanca.validation.NotaValidator;
 import VVSS_SzuhaiBoanca.validation.StudentValidator;
 import VVSS_SzuhaiBoanca.validation.TemaValidator;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Iterator;
@@ -14,26 +15,31 @@ import java.util.Iterator;
 import static org.junit.Assert.*;
 
 public class ServiceTest {
+    private StudentValidator studentValidator;
+    private TemaValidator temaValidator;
+    private String filenameStudent="fisiere/StudentiTest.xml";
+    private String filenameTema = "fisiere/TemeTest.xml";
+    private String filenameNota = "fisiere/NoteTest.xml";
+    StudentXMLRepo studentXMLRepository;
+    TemaXMLRepo temaXMLRepository;
+    NotaValidator notaValidator;
+    NotaXMLRepo notaXMLRepository;
+    Service service;
+
+    @Before
+    public void setup(){
+        studentValidator = new StudentValidator();
+        temaValidator = new TemaValidator();
+        studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        temaXMLRepository = new TemaXMLRepo(filenameTema);
+        notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        notaXMLRepository = new NotaXMLRepo(filenameNota);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
 
     @Test
     public void addStudent() {
         Student a=new Student("1","Sara",931,"sara@gmail.com");
-        StudentValidator studentValidator = new StudentValidator();
-        TemaValidator temaValidator = new TemaValidator();
-        String filenameStudent="fisiere/StudentiTest.xml";
-        String filenameTema = "fisiere/TemeTest.xml";
-        String filenameNota = "fisiere/NoteTest.xml";
-        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
-        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
-        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
-        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
-        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
-        int i1 = 0;
-        Iterator it1=service.getAllStudenti().iterator();
-        while(it1.hasNext()) {
-            i1++;
-            it1.next();
-        }
         service.addStudent(a);
         int i = 0;
         Iterator it=service.getAllStudenti().iterator();
@@ -41,23 +47,18 @@ public class ServiceTest {
             i++;
             it.next();
         }
-        assertEquals(1, i-i1);
-
+        assertEquals(1, i);
+        assertEquals(a.getNume(),service.findStudent("1").getNume());
     }
     @Test
     public void addStudent2() {
         Student a=new Student("1","Sara",931,"sara@gmail.com");
-        StudentValidator studentValidator = new StudentValidator();
-        TemaValidator temaValidator = new TemaValidator();
-        String filenameStudent="fisiere/StudentiTest.xml";
-        String filenameTema = "fisiere/TemeTest.xml";
-        String filenameNota = "fisiere/NoteTest.xml";
-        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
-        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
-        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
-        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
-        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
-        service.addStudent(a);
-        assertEquals(a.getNume(),service.findStudent("1").getNume());
+        Student ret = service.addStudent(a);
+        assertEquals(ret, a);
+    }
+
+    @Test
+    public void addStudent3() {
+
     }
 }
