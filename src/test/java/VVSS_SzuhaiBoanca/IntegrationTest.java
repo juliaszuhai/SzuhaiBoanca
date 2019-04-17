9e88a89d40157e1bf9e620a1f29b22f93e6d9302
iulia <juliaszuhai@gmail.com>
2019-04-17 10:51:28 +0300

    Big bang integration added

---
+++ src/test/java/VVSS_SzuhaiBoanca/IntegrationTest.java
@@ -0,0 +1,120 @@
+package VVSS_SzuhaiBoanca;
+
+import VVSS_SzuhaiBoanca.curent.Curent;
+import VVSS_SzuhaiBoanca.domain.Nota;
+import VVSS_SzuhaiBoanca.domain.Student;
+import VVSS_SzuhaiBoanca.domain.Tema;
+import VVSS_SzuhaiBoanca.repository.NotaXMLRepo;
+import VVSS_SzuhaiBoanca.repository.StudentXMLRepo;
+import VVSS_SzuhaiBoanca.repository.TemaXMLRepo;
+import VVSS_SzuhaiBoanca.service.Service;
+import VVSS_SzuhaiBoanca.validation.NotaValidator;
+import VVSS_SzuhaiBoanca.validation.StudentValidator;
+import VVSS_SzuhaiBoanca.validation.TemaValidator;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+
+public class IntegrationTest {
+    private StudentValidator studentValidator;
+    private TemaValidator temaValidator;
+    private String filenameStudent="fisiere/StudentiTest.xml";
+    private String filenameTema = "fisiere/TemeTest.xml";
+    private String filenameNota = "fisiere/NoteTest.xml";
+    StudentXMLRepo studentXMLRepository;
+    TemaXMLRepo temaXMLRepository;
+    NotaValidator notaValidator;
+    NotaXMLRepo notaXMLRepository;
+    Service service;
+
+    @Before
+    public void setup(){
+        studentValidator = new StudentValidator();
+        temaValidator = new TemaValidator();
+        studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        temaXMLRepository = new TemaXMLRepo(filenameTema);
+        notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        notaXMLRepository = new NotaXMLRepo(filenameNota);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    @Test
+    public void addStudent() {
+        Student a=new Student("1","Sara",931,"sara@gmail.com");
+        service.addStudent(a);
+        int i = 0;
+        Iterator it=service.getAllStudenti().iterator();
+        while(it.hasNext()) {
+            i++;
+            it.next();
+        }
+        assertEquals(1, i);
+        assertEquals(a.getNume(),service.findStudent("1").getNume());
+    }
+
+    @Test
+    public void  addAssignment(){
+        Tema a=new Tema("1","Descriere",2,1);
+        service.addTema(a);
+        int i = 0;
+        Iterator it=service.getAllTeme().iterator();
+        while(it.hasNext()) {
+            i++;
+            it.next();
+        }
+        assertEquals(1, i);
+        //assertEquals(a.getNume(),service.findStudent("1").getNume());
+    }
+
+    @Test
+    public void addGradeIntegration(){
+        Tema a=new Tema("1","Descriere",2,1);
+        service.addTema(a);
+        int k = 0;
+        Iterator it=service.getAllTeme().iterator();
+        while(it.hasNext()) {
+            k++;
+            it.next();
+        }
+        assertEquals(1, k);
+        Student sara=new Student("1","Sara",931,"sara@gmail.com");
+        service.addStudent(sara);
+        int j=0;
+        Iterator it2=service.getAllStudenti().iterator();
+        while(it2.hasNext()) {
+            j++;
+            it2.next();
+        }
+        assertEquals(1, j);
+        Nota nota=new Nota("1","1","1",10,LocalDate.now());
+        service.addNota(nota,"foarte bine");
+        int i=0;
+        Iterator it3=service.getAllNote().iterator();
+        while(it3.hasNext()) {
+            i++;
+            it3.next();
+        }
+        assertEquals(2, i);
+    }
+
+    @Test
+    public void addGrade(){
+        Tema a=new Tema("1","Descriere",2,1);
+        service.addTema(a);
+        Student sara=new Student("1","Sara",931,"sara@gmail.com");
+        service.addStudent(sara);
+        Nota nota=new Nota("1","1","1",10,LocalDate.now());
+        service.addNota(nota,"foarte bine");
+        int i=0;
+        Iterator it=service.getAllNote().iterator();
+        while(it.hasNext()) {
+            i++;
+            it.next();
+        }
+        assertEquals(2, i);
+    }
+}
