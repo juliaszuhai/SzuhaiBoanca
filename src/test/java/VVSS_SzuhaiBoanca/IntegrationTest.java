9ddc39eac16b1ee321ac677139dc49d16a91f536
saboanca <sara_boanca@yahoo.com>
2019-05-07 14:43:37 +0300

    Add lab5 take home.

--- src/test/java/VVSS_SzuhaiBoanca/IntegrationTest.java
+++ src/test/java/VVSS_SzuhaiBoanca/IntegrationTest.java
@@ -52,7 +52,7 @@ public class IntegrationTest {
             i++;
             it.next();
         }
-        assertEquals(1, i);
+        assertEquals(3, i);
         assertEquals(a.getNume(),service.findStudent("1").getNume());
     }
 
@@ -89,7 +89,7 @@ public class IntegrationTest {
             j++;
             it2.next();
         }
-        assertEquals(1, j);
+        assertEquals(3, j);
         Nota nota=new Nota("1","1","1",10,LocalDate.now());
         service.addNota(nota,"foarte bine");
         int i=0;
@@ -116,5 +116,73 @@ public class IntegrationTest {
             it.next();
         }
         assertEquals(2, i);
+    }
+
+    @Test
+    public void addStudent2() {
+        Student a=new Student("2","Sara2",931,"sara@gmail.com");
+        service.addStudent(a);
+        int i = 0;
+        Iterator it=service.getAllStudenti().iterator();
+        while(it.hasNext()) {
+            i++;
+            it.next();
+        }
+        assertEquals(3, i);
+        //assertEquals(a.getNume(),service.findStudent("1").getNume());
+    }
+
+    @Test
+    public void  addAssignment2(){
+        Student a=new Student("3","Sara3",931,"sara@gmail.com");
+        service.addStudent(a);
+        int i = 0;
+        Iterator it=service.getAllStudenti().iterator();
+        while(it.hasNext()) {
+            i++;
+            it.next();
+        }
+        assertEquals(3, i);
+        Tema a1=new Tema("1","Descriere",2,1);
+        service.addTema(a1);
+        int i1 = 0;
+        Iterator it1=service.getAllTeme().iterator();
+        while(it1.hasNext()) {
+            i1++;
+            it1.next();
+        }
+        assertEquals(1, i1);
+        //assertEquals(a.getNume(),service.findStudent("1").getNume());
+    }
+
+    @Test
+    public void addGrade2(){
+        Tema a=new Tema("1","Descriere",2,1);
+        service.addTema(a);
+        int i1 = 0;
+        Iterator it1=service.getAllTeme().iterator();
+        while(it1.hasNext()) {
+            i1++;
+            it1.next();
+        }
+        assertEquals(1, i1);
+        Student sara=new Student("1","Sara",931,"sara@gmail.com");
+        service.addStudent(sara);
+        int i = 0;
+        Iterator it=service.getAllStudenti().iterator();
+        while(it.hasNext()) {
+            i++;
+            it.next();
+        }
+        assertEquals(3, i);
+        Nota nota=new Nota("1","1","1",10,LocalDate.now());
+        service.addNota(nota,"foarte bine");
+        int i2=0;
+        Iterator it2=service.getAllNote().iterator();
+        while(it2.hasNext()) {
+            i2++;
+            it2.next();
+        }
+        assertEquals(2, i2);
     }
 }
